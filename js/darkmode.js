3ad739495227e0b4cb7243d7aadd5aac6d098bb0
AjabaGodswill <AjabaGodswill@gmail.com>
2021-06-21 00:54:24 +0100

    final commit

---
+++ js/darkmode.js
@@ -0,0 +1,14 @@
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// function myFunction() {
+//    var element = document.body;
+//    element.classList.toggle("dark-mode");
+// }
